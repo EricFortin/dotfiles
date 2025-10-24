9b5c6f5c098831a73a234f2a2cd084bb421586ce
Eric Fortin <eric.fortin2@gmail.com>
2025-10-24 07:45:31 -0400

    Automated setup a little more.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+function install_ubuntu() {
+  apt-get install bat fzf ripgrep
+}
+
+function install_arch() {
+  echo "ARCH BTW"
+}
+
+if grep -q "ID=ubuntu" /etc/os-release; then
+  install_ubuntu
+elif grep -q "ID=arch" /etc/os-release; then
+  install_arch
+fi
+
 # Clone theme for tmux
 NORD_DIR=~/.config/tmux/plugins/nordtheme
 if [ ! -d "$NORD_DIR" ]; then
   mkdir -p $NORD_DIR
   git clone https://github.com/nordtheme/tmux.git $NORD_DIR/tmux
 fi
+
+stow bash
+stow git
+stow tmux
